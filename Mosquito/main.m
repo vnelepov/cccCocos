0647d8f0d7c5c39ae8e54d1a2618817f20627ce7
Vladimir Nelepov <Nevl@vnelepov-imac.local>
2012-05-22 10:20:40 +0400

    Initial Commit

---
+++ Mosquito/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Mosquito
+//
+//  Created by Vladimir Nelepov on 22.05.12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
